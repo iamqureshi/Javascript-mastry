50deac31a46e974e97d119011f5cc6a11bf850d4
Samir Shaikh <samirshaikh.co.in@gmail.com>
2026-02-17 23:19:59 +0530

    Find Sum of Array

---
+++ sum-of-array.js
@@ -0,0 +1,13 @@
+// Find the sum of all elements in array.
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+const arr = [1, 2, 3, 4];
+
+let sum = 0;
+
+for (let item of arr) {
+  sum += item;
+}
+
+console.log(sum);
