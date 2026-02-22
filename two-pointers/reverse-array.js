d3e77bff136567c7270688305712ed887377aa13
Samir Shaikh <samirshaikh.co.in@gmail.com>
2026-02-22 20:05:05 +0530

    revers an array using moderns way

--- two-pointers/reverse-array.js
+++ two-pointers/reverse-array.js
@@ -3,13 +3,25 @@ const arr =[1,2,3,4,5];
 let left = 0;
 let right = arr.length - 1;
 
-while(left < right){
-    const temp = arr[left];
-    arr[left] = arr[right];
-    arr[right] = temp;
+// while(left < right){
+//     const temp = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = temp;
 
-    left++;
-    right--
+//     left++;
+//     right--
+// }
+
+
+function getRevers(){
+    while(left < right){
+        (arr[left], arr[right]) = (arr[right], arr[left]);
+        left++;
+        right--
+    }
+
+    return arr;
 }
 
-console.log(arr)
+// console.log(arr)
+console.log(getRevers())
