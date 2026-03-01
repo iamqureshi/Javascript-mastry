69bd3fb687d778c954e281750c73f76ac21bee7a
Samir Shaikh <samirshaikh.co.in@gmail.com>
2026-03-01 12:53:40 +0530

    pair sum

--- practice.js
+++ practice.js
@@ -1,14 +1,21 @@
-const arr = [1,2,3,4,5];
+// Pair sum
+const arr = [1,2,3,4,6];
+let target = 6;
+
 let left = 0;
-let right = arr.length-1;
+let right = arr.length - 1;
 
 while(left < right){
-    let temp = arr[left];
-    arr[left] = arr[right];
-    arr[right]= temp;
+    let sum = arr[left] + arr[right];
 
-    left++;
-    right--
+    if(sum === target){
+        console.log("Success", target, sum);
+        return
+    }
+
+    if(sum > target){
+        right--
+    }else{
+        left++
+    }
 }
-
-console.log(arr)
